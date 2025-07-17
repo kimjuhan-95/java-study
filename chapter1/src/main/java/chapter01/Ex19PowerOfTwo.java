9f4bfef028c92847c8bc9ecb2678a974d2ba360a
bit <bit@DESKTOP-8VH3FOQ>
2025-07-17 17:54:41 +0900

    drill

---
+++ chapter1/src/main/java/chapter01/Ex19PowerOfTwo.java
@@ -0,0 +1,20 @@
+package chapter01;
+
+public class Ex19PowerOfTwo {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		int result = 1;
+		
+		while(i < 10) {
+			result = result * 2;
+			
+			i++;
+		}
+		
+		System.out.println(result);
+
+	}
+
+}
---
+++ chapter1/src/main/java/chapter01/Ex30WhileContinueFlx.java
@@ -0,0 +1,28 @@
+package chapter01;
+
+public class Ex30WhileContinueFlx {
+
+	public static void main(String[] args) {
+		// int i = 0;
+//
+	//	while (i < 10) { 
+		//    if (i > 5) {
+		  //  	i++;
+		    //   continue; 
+		   //} 
+		   //System.out.println(i); 
+		   //i++; 
+	//	}
+			
+		for (int i = 0; i < 10; i++) {
+		    if ( i > 5 ) {
+		       continue;
+		    }
+		     
+		    System.out.println(i); 
+		}
+		}
+
+	}
+
+
